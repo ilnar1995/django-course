7d90df31d41e10f3b270ce52cf73e8d711aea5d4
unknown <ilnar150595@gmail.com>
2022-05-25 17:44:37 +0300

    slug2

---
+++ coolsite/women/forms.py
@@ -0,0 +1,11 @@
+from unicodedata import category
+from django import forms
+from .models import *
+
+class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    slug = forms.SlugField(max_length=255)
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols':68, 'rows':10}))
+    is_published = forms.BooleanField()
+    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+
--- coolsite1/women/views.py
+++ coolsite1/women/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
-from sympy import content
 
-from women.forms import AddPostForm
-
+from .forms import *
 from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -15,11 +13,12 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
+
     context = {
         'posts': posts,
         'menu': menu,
         'title': 'Главная страница',
-        'cat_selected': 0
+        'cat_selected': 0,
     }
 
     return render(request, 'women/index.html', context=context)
@@ -29,16 +28,19 @@ def about(request):
 
 
 def addpage(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = AddPostForm(request.POST)
-        if form.is_valid():    
+        if form.is_valid():
+            #print(form.cleaned_data)
             try:
                 Women.objects.create(**form.cleaned_data)
+                return redirect('home')
             except:
-                form.add_error(None, 'Ошибка добавления данных')
+                form.add_error(None, 'Ошибка добавления поста')
+
     else:
-        form = AddPostForm()    
-    return render(request, 'women/addpage.html', {'form':form, 'menu': menu, 'title': 'Добавление статьи'})
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -51,7 +53,7 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 def show_post(request, post_slug):
-    post =get_object_or_404(Women, slug=post_slug)         # функция вибирает из модели Women пост с первичным ключом post_id, если не находит то выдает ошибкиу 404
+    post = get_object_or_404(Women, slug=post_slug)
 
     context = {
         'post': post,
@@ -59,15 +61,21 @@ def show_post(request, post_slug):
         'title': post.title,
         'cat_selected': post.cat_id,
     }
+
     return render(request, 'women/post.html', context=context)
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
+
+    if len(posts) == 0:
+        raise Http404()
+
     context = {
         'posts': posts,
         'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': cat_id
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
     }
 
     return render(request, 'women/index.html', context=context)
+
